56246107a9c7ab4db11106fd05c13027dd4200fd
AlexandrosKap <53071962+AlexandrosKap@users.noreply.github.com>
2022-12-29 13:48:59 +0200

    better comments added reportWebVitals

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,15 +1,23 @@
 import { ReportHandler } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+  // Import the necessary things needed to run the application.
   if (onPerfEntry && onPerfEntry instanceof Function) {
+    // Do that only if onPerfEntry is a function.
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      // Gets the CLS part of the application.
       getCLS(onPerfEntry);
+      // Gets the FID part of the application.
       getFID(onPerfEntry);
+      // Gets the FCP part of the application.
       getFCP(onPerfEntry);
+      // Gets the LCP part of the application.
       getLCP(onPerfEntry);
+      // Gets the TTFB part of the application.
       getTTFB(onPerfEntry);
     });
   }
 };
 
+// Export the default report web vitals.
 export default reportWebVitals;
